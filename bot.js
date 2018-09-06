88cd9d73c0735472ef2e3974370d5b7e7a405cc5
AfroPsycho <42263654+AfroPsycho@users.noreply.github.com>
2018-09-06 09:57:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDg3MTQ1MDUwNzAzMjY1Nzk0.DnJhBQ.8GIMUGqhHPKPZpa6DqKwiJXCbZE);
+client.login(process.env.BOT_TOKEN);
